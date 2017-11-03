2e8f52b1417a123c291ff522ef2981d61c2b98c0
CD-W <32667716+CD-W@users.noreply.github.com>
2017-11-02 21:25:28 -0700

    added checkEnergy, checkAlive functions
    not fully implement.

--- Hero.cpp
+++ Hero.cpp
@@ -8,6 +8,7 @@ Hero::Hero()
   location.x = 4; //Change these 5's
   location.y = 4;
   energy = DEFAULT_ENERGY;
+  alive = true;
   whiffles = DEFAULT_WHIFFLES;
   visibilityRadius = DEFAULT_VIS;
 }
@@ -22,4 +23,21 @@ Location Hero::getLocation()
 int Hero::getVisibilityRadius()
 {
   return visibilityRadius;
+}
+
+//returns the hero's energy.  
+//Will in the future call the check alive function after removing energy
+bool Hero::checkEnergy()
+{
+  return energy;
+}
+
+//Is the hero alive?  Returns if so.
+bool Hero::checkAlive()
+{
+  if(energy==0)
+  {
+  	alive = false;
+  }
+  return alive;
 }
