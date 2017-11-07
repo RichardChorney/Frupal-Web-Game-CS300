67799fd9d19aad3d5bcde4fc26ab502cc58cd8c6
Cole Cofer <coleocofer@gmail.com>
2017-11-06 17:13:53 -0800

    Added Enumerations and updated the Type class.

---
+++ Enums.h
@@ -0,0 +1,33 @@
+///////////////////////////////////////////
+//This file contains all enumeration
+//defintions for the Frupal project.
+///////////////////////////////////////////
+
+//Type Index enumeration
+//Notice that they are all lowercase,
+//which is important because there are
+//classes with the same name.
+enum typeIndex {
+    hero = 0,
+    clue = 1,
+    powerBar = 2,
+    boulder = 3,
+    wall = 4,
+    royalDiamonds = 5,
+    chest = 6,
+    explosiveChest = 7,
+    binoculars = 8,
+    bush = 9,
+    tree = 10,
+    axe = 11,
+    chainsaw = 12
+
+};
+
+//Terrain Index enumeration
+enum terrainIndex {
+    medow = 0,
+    water = 1,
+    bog = 2,
+    forest = 3
+};
